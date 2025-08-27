3a39a75dbe5aa30616c0d0b40275d097cfb29a83
Nessrine88 <macherki.nessrine@yahoo.com>
2025-08-27 21:26:15 +0100

    install next app

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Her
+
+export default function Home() {
+  return (
+    <div className="font-bold font-oleo text-7xl flex h-screen justify-center items-center">
+    <Hero />
+    </div>
+  );
+}
