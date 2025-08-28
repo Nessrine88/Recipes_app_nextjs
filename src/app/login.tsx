a3467d21afddf8a28b07a312adad74233f8ad18a
Nessrine88 <macherki.nessrine@yahoo.com>
2025-08-28 22:29:02 +0100

    adjust the outline

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -5,8 +5,8 @@ const Login = () => {
       <h2 className="text-center  text-2xl text-gray-700 font-bold">Welcome Back</h2>
       <p className="text-center  py-5 text-lg text-gray-400">Please enter your account here </p>
 <div className="space-y-5 w-5xl">
-    <input type="email" name="Email" id="email" placeholder='Email or phone number' className="w-full bg-white py-4 pr-10 pl-3  text-left rounded-2xl "/> <br />
-     <input type="password" name="password" id="password" placeholder='password'className="w-full py-4 pr-10 pl-3 text-left rounded-2xl" />
+    <input type="email" name="Email" id="email" placeholder='Email or phone number' className="w-full bg-white py-4 pr-10 pl-3  text-left rounded-3xl  border-2 border-transparent focus:!border-green-400 focus:outline-none "/> <br />
+     <input type="password" name="password" id="password" placeholder='password' className="w-full py-4 pr-10 pl-3 text-left rounded-3xl border-2 border-transparent focus:!border-green-400 focus:outline-none" />
 </div>
  
 
